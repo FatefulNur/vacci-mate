9716af29a9d3dc61810919aae8ffb04c31df302b
FatefulNur <km.nurunnabi66@gmail.com>
2024-01-08 18:33:54 +0600

    Added model files

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,7 +3,9 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use App\Enums\UserStatus;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -20,17 +22,11 @@ class User extends Authenticatable
     protected $fillable = [
         'name',
         'email',
-        'password',
-    ];
-
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
+        'nid',
+        'phone',
+        'status',
+        'vaccine_center_id',
+        'scheduled_at',
     ];
 
     /**
@@ -40,6 +36,13 @@ class User extends Authenticatable
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
-        'password' => 'hashed',
+        'status' => UserStatus::class,
+        'nid' => 'integer',
+        'phone' => 'integer',
     ];
+
+    public function vaccineCenter(): BelongsTo
+    {
+        return $this->belongsTo(VaccineCenter::class);
+    }
 }
