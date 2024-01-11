cdf576437d68daebfa49c3b4ca7b5fc936995a08
FatefulNur <km.nurunnabi66@gmail.com>
2024-01-11 20:47:36 +0600

    Updated user model added data represent logic

--- app/Models/User.php
+++ app/Models/User.php
@@ -36,6 +36,7 @@ class User extends Authenticatable
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
+        'scheduled_at' => 'datetime',
         'status' => UserStatus::class,
         'nid' => 'integer',
         'phone' => 'integer',
@@ -44,5 +45,18 @@ class User extends Authenticatable
     public function vaccineCenter(): BelongsTo
     {
         return $this->belongsTo(VaccineCenter::class);
+    }
+
+    public function hasBeenVaccinated()
+    {
+        return ($this->status === UserStatus::SCHEDULED &&
+            !is_null($this->scheduled_at) &&
+            $this->scheduled_at->diffInHours(now()) >= 12);
+    }
+
+    public function canBeVaccinated()
+    {
+        return ($this->status === UserStatus::NOT_VACCINATED &&
+            is_null($this->scheduled_at));
     }
 }
