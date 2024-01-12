e05957924731a818d349c9ea1bdf99fbc18f6657
FatefulNur <km.nurunnabi66@gmail.com>
2024-01-12 16:10:44 +0600

    Added loading logic for registration-from livewire component

--- resources/views/livewire/registration-form.blade.php
+++ resources/views/livewire/registration-form.blade.php
@@ -5,10 +5,12 @@
             <form wire:submit="create" class="flex-1">
                 {{ $this->form }}
 
-                <button type="submit"
-                    class="px-4 py-2 block text-md font-semibold uppercase bg-amber-600 text-white hover:bg-amber-700 w-full rounded-lg mt-5 transition-all">
+                <button wire:loading.attr="disabled" type="submit"
+                    class="px-4 py-2 block text-md font-semibold uppercase bg-amber-600 text-white hover:bg-amber-700 w-full rounded-lg mt-5 transition-all disabled:bg-amber-800">
                     Submit
                 </button>
+                <div class="sr-only mx-auto text-gray-800 text-sm py-2 text-center" wire:loading.class.remove="sr-only">
+                    Loading...</div>
             </form>
         </div>
     </div>
