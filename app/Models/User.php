bd4fb9bd8112bb5892fd2a86b7267b1302e13ab4
FatefulNur <km.nurunnabi66@gmail.com>
2024-01-13 10:39:57 +0600

    Added hidden fields, authorization for panel in user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,22 +3,19 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Filament\Panel;
 use App\Enums\UserStatus;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Notifications\Notifiable;
+use Filament\Models\Contracts\FilamentUser;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     use HasApiTokens, HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
     protected $fillable = [
         'name',
         'email',
@@ -26,20 +23,22 @@ class User extends Authenticatable
         'phone',
         'status',
         'vaccine_center_id',
+        'password',
         'scheduled_at',
     ];
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
     protected $casts = [
         'email_verified_at' => 'datetime',
         'scheduled_at' => 'datetime',
         'status' => UserStatus::class,
         'nid' => 'integer',
         'phone' => 'integer',
+        'password' => 'hashed',
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
     ];
 
     public function vaccineCenter(): BelongsTo
@@ -58,5 +57,10 @@ class User extends Authenticatable
     {
         return ($this->status === UserStatus::NOT_VACCINATED &&
             is_null($this->scheduled_at));
+    }
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return in_array($this->email, ['admin@test.com']);
     }
 }
