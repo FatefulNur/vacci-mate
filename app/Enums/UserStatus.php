fda963768b9e43a5930308e761a7d1c4f37c06f4
FatefulNur <km.nurunnabi66@gmail.com>
2024-01-13 10:38:51 +0600

    Added filament color, icon in user status enum

--- app/Enums/UserStatus.php
+++ app/Enums/UserStatus.php
@@ -2,7 +2,11 @@
 
 namespace App\Enums;
 
-enum UserStatus: string
+use Filament\Support\Contracts\HasColor;
+use Filament\Support\Contracts\HasIcon;
+use Filament\Support\Contracts\HasLabel;
+
+enum UserStatus: string implements HasLabel, HasColor, HasIcon
 {
     case NOT_VACCINATED = 'not_vaccinated';
     case SCHEDULED = 'scheduled';
@@ -14,6 +18,24 @@ enum UserStatus: string
             self::NOT_VACCINATED => 'Not Vaccinated',
             self::SCHEDULED => 'Scheduled',
             self::VACCINATED => 'Vaccinated',
+        };
+    }
+
+    public function getColor(): string
+    {
+        return match ($this) {
+            self::NOT_VACCINATED => 'danger',
+            self::SCHEDULED => 'warning',
+            self::VACCINATED => 'success',
+        };
+    }
+
+    public function getIcon(): string
+    {
+        return match ($this) {
+            self::NOT_VACCINATED => 'heroicon-o-x-circle',
+            self::SCHEDULED => 'heroicon-o-exclamation-circle',
+            self::VACCINATED => 'heroicon-o-check-circle',
         };
     }
 }
