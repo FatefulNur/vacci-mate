90531e2123e6a1dd5d8b00f882d8943bb7dbf307
FatefulNur <km.nurunnabi66@gmail.com>
2024-01-16 14:56:38 +0600

    Added has many relationship of vaccine-center with users

--- app/Models/VaccineCenter.php
+++ app/Models/VaccineCenter.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class VaccineCenter extends Model
 {
@@ -13,4 +14,9 @@ class VaccineCenter extends Model
         'name',
         'limit',
     ];
+
+    public function users(): HasMany
+    {
+        return $this->hasMany(User::class);
+    }
 }
